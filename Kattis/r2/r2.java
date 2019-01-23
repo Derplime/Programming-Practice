35599d7131b88539d5980bde7517b18f2bdde811
Emil Dolorfino <edolorfino24@gmail.com>
2019-01-22 23:00:28 -0500

    adding Kattis challenge R2

---
+++ Kattis/r2/r2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class r2 {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int r1 = s.nextInt();
+        int M = s.nextInt();
+        int ret = (M * 2) - r1;
+
+        System.out.println(ret);
+    }
+}
