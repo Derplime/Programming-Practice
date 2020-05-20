50a491e16519b189b32b7068295716d0ef9ad9df
Emil Dolorfino <edolorfino24@gmail.com>
2020-05-20 19:39:37 -0400

    finished node.js module

--- Programming Courses/Udemy/Complete 2020 Web Dev Bootcamp/Lesson 18 - Node.js/index.js
+++ Programming Courses/Udemy/Complete 2020 Web Dev Bootcamp/Lesson 18 - Node.js/index.js
@@ -1,4 +1,13 @@
 // native node modules
 const fs = require("fs");
 
+// external node module
+const superheroes = require("superheroes");
+const supervillains = require("supervillains");
+
 fs.copyFileSync("file1.txt", "file2.txt");
+
+var superName =  superheroes.random();
+var villainName = supervillains.random()
+
+console.log(`${superName} vs. ${villainName}`);
